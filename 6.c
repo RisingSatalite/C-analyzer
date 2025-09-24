aa73f8cec73537be34f9ee7297cb4b61d3ccff5d
RisingSatalite <saihajmann07@gmail.com>
2025-09-24 14:16:38 -0600

    initial

---
+++ 6.c
@@ -0,0 +1,4 @@
+int loyaltyPoints = 200;
+int purchasePoints = 50;
+loyaltyPoints = purchasePoints; 
+System.out.println("Updated Loyalty Points: " + loyaltyPoints);
---
+++ analyzer.py
@@ -0,0 +1,47 @@
+file = open("6.c", "r")
+
+broken_up_list = []
+string_holder = ""
+is_in_string = False
+
+for line in file:
+    print(line.strip()) 
+
+    line = line.replace("("," ( ")
+    line = line.replace(")"," ) ")
+    line = line.replace(":"," : ")
+    line = line.replace(";"," ; ")
+    line = line.replace("."," . ")
+    print(line)
+    tokens = line.split(" ")
+
+    for token in tokens:
+        print(token)
+        try:
+            if(token[0] == '"'):
+                is_in_string = True
+        except IndexError:
+            pass#Probably a space
+        if(is_in_string):
+            string_holder += token
+            try:
+                if(token[-1] == '"'):
+                    is_in_string = False
+                    broken_up_list.append(string_holder)
+                    string_holder = ""
+            except IndexError:
+                pass#Probably a space
+        else:
+            broken_up_list.append(token)
+
+if(is_in_string):
+    print("Error, string is opened, but no closed")
+
+varibles = []
+
+for token in broken_up_list:
+    print(token)
+    if(token == "int"):
+        pass
+
+
