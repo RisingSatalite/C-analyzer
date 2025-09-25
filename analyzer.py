061b3a310f96d3601fafef024c8a0b5df88a75b9
RisingSatalite <saihajmann07@gmail.com>
2025-09-25 10:19:23 -0600

    better single loop

--- analyzer.py
+++ analyzer.py
@@ -4,13 +4,14 @@ broken_up_list = []
 string_holder = ""
 is_in_string = False
 previous_token = ''
-lineCount = 1
-latestLine = 1
+lineCount = 0#Start at 0, but increased to 1 immediately in loop
+latestLine = 0
 varibles = []
 getting_varible_name = False
 dataTypes = ['int','short','long','float','double','char','void']
 
 for line in file:
+    lineCount += 1
     print(line.strip()) 
 
     line = line.replace("("," ( ")
@@ -23,35 +24,41 @@ for line in file:
 
     for token in tokens:
         print(token)
-        try:
-            if(token[0] == '"'):
-                is_in_string = True
-        except IndexError:
-            pass#Probably a space
+
+        if(not is_in_string):
+            try:
+                if(token[0] == '"'):
+                    is_in_string = True
+                    print("String opened")
+            except IndexError:
+                pass#Probably a space
+
+
         if(is_in_string):
             string_holder += token
             try:
                 if(token[-1] == '"'):
+                    print("String closed")
                     is_in_string = False
                     broken_up_list.append(string_holder)
                     string_holder = ""
+                    latestLine = lineCount
             except IndexError:
                 pass#Probably a space
+        if(getting_varible_name):
+            varibles.append([token, getting_varible_name])
+            getting_varible_name = False
+            print('Varible name determined')
+        if(token in dataTypes):
+            print('Varible declaration detected')
+            getting_varible_name = token
+            continue
         else:
             broken_up_list.append(token)
+    latestLine = lineCount
 
 if(is_in_string):
-    print("Error, string is opened, but not closed")
-
-for token in broken_up_list:
-    print(token)
-    if(getting_varible_name):
-        varibles.append([token, getting_varible_name])
-        getting_varible_name = False
-    elif(token in dataTypes):
-        getting_varible_name = token
-        continue
+    print(f"Error, string is opened at line {latestLine}, but not closed even at the end of program")
 
 for varible in varibles:
     print(varible[0], varible[1])
-
